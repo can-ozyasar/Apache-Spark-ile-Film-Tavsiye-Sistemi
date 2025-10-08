88d45a0aa8a0ed353b880742e82d8aaca05287b2
Ozyasar <canozyasar35@gmail.com>
2025-10-08 10:04:25 +0300

    Model dosyasının hazırlanması,yz modelini eğiten ve kaydeden kodlar eklendi.

--- trainModel.py
+++ trainModel.py
@@ -24,5 +24,47 @@ def main():# tüm kodlarımızı bu fonksiyonun içine yazacağız
 
     ratings_df=ratings_df.withColumn("userId",col("userId").cast(IntegerType()))    
     ratings_df=ratings_df.withColumn("movieId",col("movieId").cast(IntegerType()))
-    ratings_df=ratings_df.withColumn("rating",col("rating").cast(IntegerType()))    
+    ratings_df=ratings_df.withColumn("rating",col("rating").cast(FloatType()))
 
+
+    # Model seçimi İÇİN En güçlü ve en popüler algoritmalardan biri olan ALS (Alternating Least Squares) algoritmasını kullanacağız.
+    # ALS, özellikle büyük veri setlerinde ve seyrek matrislerde iyi performans göster
+    
+    
+    ##?#  temel mantık sena benzer zevkte insanların beğendiği filmleri sana da önerecek     
+    
+
+    als=ALS(
+        maxIter=10,# max kaç iterasyon çalışacağını belirler öğreneme aşamasında veri üzerinden en fazla 10 defa geçsin demektir
+        regParam=0.1, # regularization parametresi overfitting i önlemek için kullanılır aşırı öğrenirse sadece o veri seti ile çalışacak seviyeye gelir ve yeni verilerde başarısız olur
+        userCol="userId", # kullanıcı id sini belirler
+        itemCol="movieId", # film id sini belirler  
+        ratingCol="rating", # kullanıcıların filmlere verdiği puanları belirler
+        coldStartStrategy="drop" #soğuk başlangıç, modelin eğitim sırasında görmediği kullanıcılar veya filmler için tahmin yaparken hata vermemesi için kullanılır
+        # eğer ilk defa karşılaşırsa filmle yada bir kullanıcıya o kaydı drop yap diyoruz
+
+    )
+
+    #MODELİ EĞİTME 
+    model =als.fit(ratings_df)
+    model_path="als_model"    # modeli eğitmek zaman alan bir iş olduğu için her tavsiye için tekrar modeli eğitmek yerine bir defa eğitip kaydetmek daha mantıklı
+    model.write().owerwrite().save(model_path) # modeli kaydettik
+
+
+
+    movies_parquet_path="movies_parquet" # movies verisetini parquet formatında kaydedeceğiz daha hızlı okuma ve yazma işlemi için
+    movies_df.write.mode("overwrite").parquet(movies_parquet_path) # overwrite var ise üzerine yaz yoksa oluştur demek  
+
+    # ekrana model başarıllı şekilde eğitildi yazdırma
+    print(f"Model başarıyla eğitildi ve {model_path} , klasörüne kaydedildi.")
+    print(f"filmler verisi{movies_parquet_path} klasörüne  kaydedildi")
+
+    spark.stop() # spark session ı kapatıyoruz
+
+
+
+
+
+# main fonksiyonu çalıştırma
+if __name__ == "__main__":
+    main()
